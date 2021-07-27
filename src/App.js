4d29a56da12d37b4244b9a094601399cb4bfb087
Joseph Xiao <josephxiao8@gmail.com>
2021-07-27 19:31:03 -0400

    revised code with useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './index.css';
 import Activity from './Activity';
 
-function App() {
 
-    const [todo, setTodo] = useState([]); //maintains a list of entries in the todo list
-    const [cnt, setCnt] = useState(0); //to generate a new value for the key attribute in elements in 'todo'
+const reducer = (state, action) => {
+    if(action.type==='add'){
+        return (
+            {todo: [...(state.todo), {id: state.cnt+1, message:action.value}], cnt: state.cnt+1}
+        )
+    }
+    else if(action.type==='remove'){
+        const newTodo = state.todo.filter((entry)=>entry.id!=action.id);
+        return (
+            {todo: newTodo, cnt: state.cnt}
+        )
+    }
+    else if(action.type==='clear'){
+        return (
+            {todo: [], cnt: 0}
+        )
+    }
+};
+
+function App() {
+    
     const [entryText, setEntryText] = useState(''); //maintains the text inside the form for new entries
     const [showForm, setShowForm] = useState(false); //true-show form, false-hide form
     const [showColorOptions, setShowColorOptions] = useState(false); //true-show popup, false-hide pop-up
+    const [color, setColor] = useState('red'); //the current color theme
+
+    //state.todo maintains a list of entries in the todo list
+    //state.cnt generates a new value for the key attribute in elements in 'todo'
+    const [state, dispatch] = useReducer(reducer, {todo:[], cnt:0});
 
     //adds new element to 'todo'
-    const add = (event) => { 
+    const add = (event) => {
         event.preventDefault();
         if(entryText) {
-            setCnt(cnt+1);
-            setTodo((prevTodo) => {
-                return [...prevTodo, {id: cnt, message: entryText}]
-            });
+            dispatch({type:'add', value:entryText});
             setEntryText('');
         }
         setShowForm(false);
@@ -25,19 +45,18 @@ function App() {
 
     //removes element in 'todo'
     const remove = (id) => {
-        setTodo((prevTodo) => {
-            return prevTodo.filter((activity) => {return activity.id!==id;});
-        });
-    }
+        dispatch({type:'remove', id:id});
+    };
 
-    //the current color theme
-    const [color, setColor] = useState('red');
+    const clear = () => {
+        dispatch({type:'clear'});
+    }
 
     //changes the color theme
     const changeColor = (col) => {
         setColor(col);
         setShowColorOptions(false);
-    }
+    };
 
 
     return (
@@ -62,7 +81,7 @@ function App() {
                 </div>
                 <div className='space-y-8'>
                     {
-                        todo.map((activity) => {
+                        state.todo.map((activity) => {
                             return(
                                 <Activity key={activity.id} message={activity.message} onClick={()=>remove(activity.id)} color={color}></Activity>
                             )
@@ -74,7 +93,7 @@ function App() {
             {/* buttons for commands */}
             <div className='flex w-3/6 mx-auto'>
                 <button id='btn1' type='button' className={`flex-1 rounded-lg bg-${color}-200 shadow-sm p-4 mx-2 transform hover:scale-105`} onClick={()=>setShowForm(!showForm)}>Add</button>
-                <button id='btn2' type='button' className={`flex-1 rounded-lg bg-${color}-200 shadow-sm p-4 mx-2 transform hover:scale-105`} onClick={()=>setTodo([])}>Clear</button>
+                <button id='btn2' type='button' className={`flex-1 rounded-lg bg-${color}-200 shadow-sm p-4 mx-2 transform hover:scale-105`} onClick={clear}>Clear</button>
                 <button id='btn3' type='button' className={`flex-1 rounded-lg bg-${color}-200 shadow-sm p-4 mx-2 transform hover:scale-105`} onClick={()=>setShowColorOptions(true)}>Change Color</button>
             </div>
 
@@ -90,9 +109,7 @@ function App() {
                 </div>
             )}
 
-
             <br></br>
-
         </div>
     );
 }
